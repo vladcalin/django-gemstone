a584c9556699e0172e91188351b0049a37aa0c0d
Calin Vlad <vlad.s.calin@gmail.com>
2017-05-11 20:15:34 +0300

    added some comments

--- django_gemstone/views.py
+++ django_gemstone/views.py
@@ -10,22 +10,31 @@ from .errors import JsonRpcMethodNotFoundError, JsonRpcInternalError
 
 
 class JsonRpcEndpoint(View):
+    """
+    The main view class for the JSON RPC endpoint.
+    """
+
     def post(self, request):
+        # checks the content type
         if request.content_type not in ("application/json",):
             return HttpResponseBadRequest("Invalid content type")
 
+        # parse the request
         jsonrpc_req = get_jsonrpc_request_from_http_request(request)
 
+        # check if the method exists
         try:
             method_obj = methods.get_method_by_name(jsonrpc_req.method)
         except ValueError:
             raise JsonRpcMethodNotFoundError("Method '{}' not found".format(jsonrpc_req.method))
 
+        # actual method call
         try:
             resp = method_obj.call_with_params(jsonrpc_req.params)
         except Exception as e:
             raise JsonRpcInternalError(e)
 
+        # return the json rpc response in case of success
         return JsonResponse({
             "jsonrpc": "2.0",
             "id": jsonrpc_req.id,
@@ -35,4 +44,8 @@ class JsonRpcEndpoint(View):
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-        return super(JsonRpcEndpoint, self).dispatch(request, *args, **kwargs)
+        try:
+            return super(JsonRpcEndpoint, self).dispatch(request, *args, **kwargs)
+        except Exception as e:
+            # TODO: handle json rpc specific errors here
+            pass
